ef2785ca4dc540fb3b08edc2426ebcf532789f96
Spalla88 <aguspalla88@gmail.com>
2024-10-28 18:25:51 -0300

    update models

--- AppConci/models.py
+++ AppConci/models.py
@@ -38,29 +38,3 @@ class Comentario(models.Model):
     def _str_(self):
         return '%s - %s' % (self.nombre, self.comentario)
     
-
-# class Cliente(models.Model):
-#     nombre_completo = models.CharField(max_length=100)
-#     cuit = models.CharField(max_length=11)
-#     email = models.EmailField()
-#     telefono = models.CharField(max_length=15)
-#     localidad = models.CharField(max_length=100)
-
-#     def _str_(self):
-#         return self.nombre_completo
-
-# class Tractor(models.Model):
-#     familia = models.CharField(max_length=100)
-#     modelo = models.CharField(max_length=100)
-#     serie = models.CharField(max_length=100)
-
-#     def _str_(self):
-#         return f"{self.familia} {self.modelo}"
-
-# class Cosechadora(models.Model):
-#     familia = models.CharField(max_length=100)
-#     modelo = models.CharField(max_length=100)
-#     serie = models.CharField(max_length=100)
-
-#     def _str_(self):
-#         return f"{self.familia} {self.modelo}"
